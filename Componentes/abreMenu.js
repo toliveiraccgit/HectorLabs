d18c28a7b4df2fa7ec8171bc7bd823d44b6b9765
Diogo-Ol <oliveiradiogo50@gmail.com>
2022-04-22 09:53:50 -0300

    Finalizando animação de menu

--- Componentes/abreMenu.js
+++ Componentes/abreMenu.js
@@ -4,18 +4,33 @@ const botaoAbreMenu = document.querySelector('#botao-menu-abre')
 
 const botaoFechaMenu = document.querySelector('#botao-menu-fecha')
 
+const vetor1 = document.querySelector('#vetor-1-menu')
+const vetor2 = document.querySelector('#vetor-2-menu')
+const vetor3 = document.querySelector('#vetor-3-menu')
+
 function abreMenu(){
     menuAberto.style.display = ` flex `
+    menuAberto.style.animation = ` animaMenu .3s ease-in-out forwards `
 
-    botaoAbreMenu.style.display = ` none `
+    vetor1.style.animation = ` animaVetor-1 .35s ease forwards `
+    vetor2.style.animation = ` animaVetor-2 .6s ease forwards `
+    vetor3.style.animation = ` animaVetor-3 .35s ease forwards `
 
-    botaoFechaMenu.style.display = ` block `
+    setTimeout(() => botaoAbreMenu.style.display = ` none `, 500);
+
+    setTimeout(() => botaoFechaMenu.style.display = ` block `, 500);
 }
 
 function fechaMenu(){
-    menuAberto.style.display = ` none `
+    menuAberto.style.animation = ` animaMenu-fecha .3s ease-in-out forwards `
+    setTimeout(() => menuAberto.style.display = ` none `, 400);
 
-    botaoAbreMenu.style.display = ` block `
+
+    vetor1.style.animation = ` animaVetor-1-fecha .35s ease forwards `
+    vetor2.style.animation = ` animaVetor-2-fecha .4s ease forwards `
+    vetor3.style.animation = ` animaVetor-3-fecha .35s ease forwards `
+
+    botaoAbreMenu.style.display = ` flex `
 
     botaoFechaMenu.style.display = ` none `
 }
